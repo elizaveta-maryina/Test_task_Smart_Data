ec578af7f630b9423f1ce3ff46eda7f040d78bc7
Елизавета Марьина <113649901+elizaveta-maryina@users.noreply.github.com>
2022-09-19 17:19:04 +0000

    Реализованы сущности авторы и книги

--- replit.nix
+++ replit.nix
@@ -1,6 +1,7 @@
 { pkgs }: {
 	deps = [
-		pkgs.php80
+		pkgs.sqlite.bin
+  pkgs.php80
         pkgs.php80Packages.composer
 	];
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::apiResources([
+    'author' => AuthorController::class,
+    'book' => BookController::class,
+    'user' => UserController::class
+    // 'posts' => PostController::class,
+]);
 });
