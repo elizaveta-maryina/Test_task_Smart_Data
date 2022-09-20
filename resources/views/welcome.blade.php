b4100c63859bb55d5dec05018559a76327c80d9f
Елизавета Марьина <113649901+elizaveta-maryina@users.noreply.github.com>
2022-09-20 18:54:45 +0000

    База данных запоняется с помощью postman

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  
     <head>
+        <link href="https://replit.com/@IelizavietaMari/Testtask#resources/css/app.css" rel="stylesheet" type="text/css">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -11,15 +13,50 @@
 
     </head>
     <body>
-        <div>
+
+      <style type="text/css">
+        body {
+
+        font-family: "Muller", sans-serif;
+
+        background-color: #e5e5e5;
+        }
+
+        a {
+        color: inherit;
+        text-decoration: none;
+        }
+        html {
+          box-sizing: border-box;
+        }
+        nav {
+        justify-content: space-around;
+        
+        } 
+        div {
+        display: inline-block;
+        padding: 0px 45px 0px 0px;
+        }
+        
+      a {
+        display: inline-block;
+        color: #666;
+        font-size: 16px;
+        line-height: 1;
+        
+        justify-content: space-around;
+        outline: none;
+      }
+
+
+      </style>
           <nav>
-          <a href="{{route('welcome-form')}}">Главная></a>
-          <a href="{{route('form_authors-form')}}">Форма></a>
-          <a href="{{route('author.index')}}">Список авторов></a>
-          <a href="{{route('book.index')}}">Список книг с указанием автора></a>
-          <a href="{{route('list-form')}}">Список книг></a>
+          <div><a href="{{route('welcome-form')}}">Main</a></div>
+          <div><a href="{{route('form_authors-form')}}">Form</a> </div>
+          <div><a href="{{route('author.index')}}">Authors</a> </div>
+          <div><a href="{{route('book.index')}}">Books</a></div>
+          <a href="{{route('list-form')}}">Update</a>
           </nav>
-        </div>
                               
     </body>
 </html>
